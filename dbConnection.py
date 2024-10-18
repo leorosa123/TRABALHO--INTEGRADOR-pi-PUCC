6e38b4537f89c6ecfc2bc98ffda30ba7e1c87f8c
César "kaiser" Campos <cesarhrccontato@gmail.com>
2024-10-18 10:40:15 -0300

    Comments for development

--- dbConnection.py
+++ dbConnection.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import flask_mysqldb as db
 
 app = Flask(__name__)
 
 @app.route("/")
 def helloWorld():
     return "<p>Hello!!</p>"
+
+# DB Connection
+# CRUD de informacoes dos usuarios
+# Tomada de customizacoes dentro do site para menu e afins
+# Validacao do login e informacoes
+# Tomada dos horarios e psicologos disponiveis dentro do banco
+# CRUD agendamento
+# Tomada de realizacao dentro do banco de dados
+# Tomada de seguranca dentro da pagina
