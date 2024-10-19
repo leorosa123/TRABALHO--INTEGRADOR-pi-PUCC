5db511d730fc0434a5fce3f3c15e42839a39993c
César "kaiser" Campos <cesarhrccontato@gmail.com>
2024-10-19 16:40:23 -0300

    ConnectionWithTheServer

--- dbConnection.py
+++ dbConnection.py
@@ -1,13 +1,19 @@
-from flask import Flask
-import flask_mysqldb as db
+from flask import Flask, render_template, request 
+from flask_mysqldb import MySQL
+import json
 
-app = Flask(__name__)
-
-@app.route("/")
-def helloWorld():
-    return "<p>Hello!!</p>"
+app = Flask(__name__) # Creating flask app
 
 # DB Connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'BancoDeDadosHOPE'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+
+mySql = MySQL(app) # SQL Connection
+
+cursor = mySql.connection.cursor() # Cursor do SQL --> usados to execute 
+
 # CRUD de informacoes dos usuarios
 # Tomada de customizacoes dentro do site para menu e afins
 # Validacao do login e informacoes
