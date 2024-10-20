7d2aace6606b813a9f4f1f69d7fcf71674aa8c80
César "kaiser" Campos <cesarhrccontato@gmail.com>
2024-10-20 11:53:39 -0300

    defing route

--- dbConnection.py
+++ dbConnection.py
@@ -1,19 +1,24 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, redirect,  url_for
 from flask_mysqldb import MySQL
 import json
 
 app = Flask(__name__) # Creating flask app
+app.config['JSON_SORT_KEYS'] = False
 
 # DB Connection
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'BancoDeDadosHOPE'
+app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_DB'] = 'BancoDeDadosHOPE'
 
 mySql = MySQL(app) # SQL Connection
 
 cursor = mySql.connection.cursor() # Cursor do SQL --> usados to execute 
 
+# Base de redirecionamento dentro das paginas
+@app.route('/')
+def index():
+    return render_template('index.html')
 # CRUD de informacoes dos usuarios
 # Tomada de customizacoes dentro do site para menu e afins
 # Validacao do login e informacoes
