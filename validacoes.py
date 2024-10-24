aa92fbda209db465057e0375af1cfa76571b2caf
César "kaiser" Campos <cesarhrccontato@gmail.com>
2024-10-24 16:20:35 -0300

    Part1: placing point and doing advertences

--- validacoes.py
+++ validacoes.py
@@ -1,2 +1,18 @@
-def validacaoLogin(email, senha, localClient):
-    if email == and senha == 
+from flask import Flask, render_template, request, redirect,  url_for
+from flask_mysqldb import MySQL
+import json
+
+def validacaoLogin(email, senha, banco:"MySQL"):
+    if email in banco and senha == banco.login.email.senha:
+        return 1
+    return 0
+
+def validaCPF(CPF:"str", founder):
+    if CPF in founder:
+        return 1
+    return 0
+
+def validaEmailCadastrado(email:"str", cpf:"str", banco:"MySQL"):
+    if email in banco.email or cpf in banco.cpf:
+        return 1
+    return 0
