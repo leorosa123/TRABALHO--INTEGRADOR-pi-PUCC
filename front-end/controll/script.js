ce83f6add4e852a5354e3bda2672d757a7ee1530
César "kaiser" Campos <cesarhrccontato@gmail.com>
2024-10-29 08:34:26 -0300

    starting JS

---
+++ front-end/controll/script.js
@@ -0,0 +1,33 @@
+const client = {
+    nome:int,
+    sessao:char,
+    modelo:char,
+    email:char,
+    id:int,
+    senha:NaN
+}
+
+function showPsicologo(){
+
+    
+
+}
+
+function selectPsicologo(){
+
+
+
+}
+
+function deployScheduling(){
+
+
+
+}
+
+
+function transformClientInformation(){
+
+
+
+}
