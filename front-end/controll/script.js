b6bbfaf8107bbc39a9b7a8d5c281a33f122f4c88
César "kaiser" Campos <cesarhrccontato@gmail.com>
2024-10-31 15:23:05 -0300

    Scripting starting

---
+++ front-end/controll/script.js
@@ -0,0 +1,6 @@
+// Recebendo post do python -> Validacao dos dados e admissao do client
+// Restricoes de login
+// Reabilitacao das restricoes
+// Mudanca de pagina com o login
+// Post para o python -> inserir dados
+// Post para o python -> transformar dados
--- mainApplication.py
+++ mainApplication.py
@@ -1,11 +1,26 @@
 # Structural import
-from flask import Flask, render_template, request, redirect,  url_for
+from flask import Flask, render_template, request, jsonify
 from flask_mysqldb import MySQL
-import json
-
-# Importing packeges and necessary functions
-from client import loggedClient
-import validacoes
 from dbConnection import mySql
 
-cursor = mySql.connection.cursor()
+app = Flask(__name__)
+
+# Rota para renderizar o formulário
+@app.route('/')
+def index():
+    return render_template('../front-end/view/index.html')
+
+# Rota para receber dados via POST do JavaScript
+@app.route('/enviar', methods=['POST'])
+def enviar_dados():
+    # Pegando as informacoes dentro do json
+    data = request.get_json()
+    nome = data.get('nome')
+    email = data.get('email')
+
+    # Jogando as informacoes dentro do banco de dados
+
+    return jsonify({'message': 'Dados salvos com sucesso!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
